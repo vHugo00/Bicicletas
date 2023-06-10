887b61f6721bf7bb89e091656cc34cb3489030c8
Vitor Hugo <vv001226@gmail.com>
2023-06-09 22:07:56 -0300

    iniciando js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+// ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativaProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativaProduto);
