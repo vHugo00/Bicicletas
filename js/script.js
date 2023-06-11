c3d7047b9edcb6c457986f4e9f74ed8d611cf567
Vitor Hugo <vv001226@gmail.com>
2023-06-11 11:40:44 -0300

    otimizado para producao

--- js/script.js
+++ js/script.js
@@ -37,3 +37,26 @@ function eventoPerguntas(pergunta) {
   pergunta.addEventListener("click", ativaPergunta);
 }
 perguntas.forEach(eventoPerguntas);
+
+//galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
